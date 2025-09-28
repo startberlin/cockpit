76fa5c958556e49f0f83dc8eeabd2d369791d9d3
Sönke Peters <soenke@soenkep.com>
2025-09-29 00:27:15 +0200

    refactor: remove middleware for session authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,22 @@ import { headers } from "next/headers";
 import { type NextRequest, NextResponse } from "next/server";
 import { auth } from "@/lib/auth";
 
+const publicRoutes = ["/auth"];
+
 export async function middleware(request: NextRequest) {
+  if (publicRoutes.includes(request.nextUrl.pathname)) {
+    return NextResponse.next();
+  }
+
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
+  console.log({ session });
+
   if (!session) {
     const redirectUrl = new URL("/auth", request.url);
+    console.log({ request: request.nextUrl.pathname });
     redirectUrl.searchParams.set("redirect", request.nextUrl.pathname);
     return NextResponse.redirect(redirectUrl);
   }
@@ -18,5 +27,8 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   runtime: "nodejs",
-  matcher: ["!/auth"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|api/auth|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+  ],
 };
