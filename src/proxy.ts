11d040711de2a51c2ba50dbf3b3f5acfd5113f53
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2026-02-01 14:33:42 +0100

    fix: npm run lint --fix

--- src/proxy.ts
+++ src/proxy.ts
@@ -37,5 +37,5 @@ export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|api|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-  ]
+  ],
 };
