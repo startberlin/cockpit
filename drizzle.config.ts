078fafa224bdd64eeb67fde73c0f86861568748e
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2026-05-10 13:45:44 +0200

    Fix preview deploys

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,16 @@
-import { env } from "@/env";
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
+
+const url = process.env.DATABASE_URL;
+if (!url) {
+  throw new Error("DATABASE_URL is required to run drizzle-kit");
+}
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url,
   },
 });
