892e82ba196e5c75c09ba4976814b60686de9f6a
Sönke Peters <soenke@soenkep.com>
2026-05-14 20:08:52 +0200

    fix: make tooltips clickable

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,12 @@ export const handleError = (error: unknown): void => {
   const message = parseError(error);
   toast(message);
 };
+
+export function useHasHover() {
+  try {
+    return matchMedia("(hover: hover)").matches;
+  } catch {
+    // Assume that if browser too old to support matchMedia it's likely not a touch device
+    return true;
+  }
+}
